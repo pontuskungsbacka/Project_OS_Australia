55f5477a7285d6b7c3625dad07af9707d2871328
Pontus <pontuskungsbacka@gmail.com>
2025-11-24 14:11:42 +0100

    updated the layout and information

--- pages/about.py
+++ pages/about.py
@@ -9,45 +9,121 @@ PAGE_TITLE = "Om oss"
 
 dash.register_page(__name__, name=PAGE_TITLE, title=f"{PAGE_TITLE} | {TITLE}", path="/about", order=8)
 
-
-layout = [
-    html.H3("Om projektet & skaparna", className="mb-3"),
-    html.P(
-        [
-            "Den här applikationen är skapad av ",
-            html.A("Adam", href="Länk till Adam LinkedIn eller portfolio"),
-            html.A("Julia", href="Länk till Julia LinkedIn eller portfolio"),
-            html.A("Laura", href="Länk till Laura LinkedIn eller portfolio"),
-            html.A("Pontus Johansson", href="Länk till Pontus LinkedIn eller portfolio"),
-        ]
-    ),
-    html.H5("Källförteckning: "),
-    html.Ul(
-        [
-            html.Li(
-                [
-                    html.B("Ikoner: "),
-                    "-",
-                    html.A("-", href="https://creativecommons.org/licenses/by-sa/3.0"),
-                    ".",
-                ],
-            ),
-            html.Li(
-                [
-                    html.B("Dataset: "),
-                    "Jerry Ng on ",
-                    html.A("Kaggle", href="https://www.kaggle.com/datasets/ngshiheng/michelin-guide-restaurants-2021"),
-                    ".",
-                ],
-            ),
-        ]
-    ),
-    html.H5("Code"),
-    html.P(
-        [
-            "The code for this app is available on ",
-            html.A("Github", href="https://github.com/niekvleeuwen/michelin-guide-restaurants-dashboard"),
-            ".",
-        ]
-    ),
-]
+def layout():
+    return [
+        html.H3("Om projektet & skaparna", className="mb-3"),
+        html.P(
+           """Detta projekt är en del av en grupp uppgift i Databehandling kursen 
+        """
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4(
+                                    "Adam Hedlund",
+                                    className="card-title",
+                                ),
+                                html.P(
+                                    [
+            "Kontakta via ",
+            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
+            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                ),
+                            ]
+                        ),
+                    ),
+                    class_name="mb-3",
+                    md=3,
+                    sm=12,
+                ),
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4(
+                                    "Julia Sälde",
+                                    className="card-title",
+                                ),
+                                html.P(
+                                    [
+            "Kontakta via ",
+            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
+            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                ),
+                            ]
+                        ),
+                    ),
+                    class_name="mb-3",
+                    md=3,
+                    sm=12,
+                ),
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4("Laura-Mirella Lomakin", className="card-title"),
+                                html.P(
+                                    [
+            "Kontakta via ",
+            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
+            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                ),
+                            ]
+                        ),
+                    ),
+                    class_name="mb-3",
+                    md=3,
+                    sm=12,
+                ),
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4("Pontus Johansson", className="card-title"),
+                                html.P(
+                                    [
+                                        "Kontakta via ",
+                                        html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
+                                        html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                ),
+                            ]
+                        ),
+                    ),
+                    class_name="mb-3",
+                    md=3,
+                    sm=12,
+                ),
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H5("Github Repository"),
+                                html.P(
+                                    [
+                                        "Här hittar ni Github Repo för detta grupparbetet ",
+                                        html.A("Github", href="https://github.com/pontuskungsbacka/Project_OS_Australia"),
+                                        ".",
+                                    ]
+                                ),
+                            
+                                html.H5("Dataset"),
+                                html.P(
+                                    [
+                                        "Datasetet som använde i projektet är hämtat från Kaggle. Datasetet är skapat av rgriffin och kan hittas här: ",
+                                        html.A("Kaggle", href="https://www.kaggle.com/code/heesoo37/olympic-history-data-a-thorough-analysis"),
+                                        ".",
+                                    ]
+                                ),
+                            ]
+                        ),
+                    ),
+                    class_name="mb-3",
+                    width=12,
+                ),
+            ],
+            class_name="g-3",
+        ),
+    ]
