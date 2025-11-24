b36af7d35f284f6dbd4d1a36ce29b0be6edfbaae
Pontus <pontuskungsbacka@gmail.com>
2025-11-24 21:48:15 +0100

    added flat profile dummy image, github & linkedin icon with link.

--- pages/about.py
+++ pages/about.py
@@ -5,6 +5,7 @@ from dash import Input, Output, callback, dcc, html
 
 TITLE = "Olympiska spelen Analys - Team Australien"
 OS_LOGO = "assets/olympic-logo.svg"
+AUS_LOGO = "assets/img/team-AUS-logo.svg"
 PAGE_TITLE = "Om oss"
 
 dash.register_page(__name__, name=PAGE_TITLE, title=f"{PAGE_TITLE} | {TITLE}", path="/about", order=8)
@@ -19,78 +20,138 @@ def layout():
         dbc.Row(
             [
                 dbc.Col(
-                    dbc.Card(
+                    dbc.Card([
+                        dbc.CardImg(src="/assets/img/profile.svg", top=True, style={"width": "100%", "height": "auto", "fill": "#0D5257"}),
                         dbc.CardBody(
                             [
                                 html.H4(
-                                    "Adam Hedlund",
+                                    "Adam Hedlund        ",
                                     className="card-title",
                                 ),
-                                html.P(
-                                    [
-            "Kontakta via ",
-            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
-            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                html.A(
+                                    href="https://www.linkedin.com/in/adam-hedlund-76492a88/",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Adam Hedlund Linkedin",
+                                            src="/assets/img/linkedin-logo.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
                                 ),
-                            ]
-                        ),
+                                html.A(
+                                    href="https://github.com/Adamhedlund",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Adam Hedlund Github",
+                                            src="/assets/img/github-mark.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
+                                ), 
+                            ], className="mh-100", style={"height": "8rem"}
+                        ),]
                     ),
                     class_name="mb-3",
                     md=3,
                     sm=12,
                 ),
                 dbc.Col(
-                    dbc.Card(
+                    dbc.Card([
+                        dbc.CardImg(src="/assets/img/profile.svg", top=True, style={"width": "100%", "height": "auto", "color": "#0D5257", "fill": "#3B7F7F"}),
                         dbc.CardBody(
                             [
                                 html.H4(
-                                    "Julia Sälde",
+                                    "Julia Sälde        ",
                                     className="card-title",
                                 ),
-                                html.P(
-                                    [
-            "Kontakta via ",
-            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
-            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                html.A(
+                                    href="https://www.linkedin.com/in/julia-s%C3%A4lde-571129381/",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Julia Sälde Linkedin",
+                                            src="/assets/img/linkedin-logo.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
                                 ),
-                            ]
-                        ),
+                                html.A(
+                                    href="https://github.com/JuliaMoa",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Julia Sälde Github",
+                                            src="/assets/img/github-mark.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
+                                ),
+                            ], className="mh-100", style={"height": "8rem"}
+                        ),]
                     ),
                     class_name="mb-3",
                     md=3,
                     sm=12,
                 ),
                 dbc.Col(
-                    dbc.Card(
+                    dbc.Card([
+                        dbc.CardImg(src="/assets/img/profile.svg", top=True, style={"width": "100%", "height": "auto", "color": "#0D5257", "fill": "#3B7F7F"}),
                         dbc.CardBody(
                             [
                                 html.H4("Laura-Mirella Lomakin", className="card-title"),
-                                html.P(
-                                    [
-            "Kontakta via ",
-            html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
-            html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                html.A(
+                                    href="https://www.linkedin.com/in/laura-mirella-lomakin-0258532a3/",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Laura-Mirella Lomakin Linkedin",
+                                            src="/assets/img/linkedin-logo.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
                                 ),
-                            ]
-                        ),
+                                html.A(
+                                    href="https://github.com/lauralomakin-hub",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Laura-Mirella Lomakin Github",
+                                            src="/assets/img/github-mark.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
+                                ),
+                            ],className="mh-100", style={"height": "8rem"}
+                        ),]
                     ),
                     class_name="mb-3",
                     md=3,
                     sm=12,
                 ),
                 dbc.Col(
-                    dbc.Card(
+                    dbc.Card([
+                        dbc.CardImg(src="/assets/img/profile.svg", top=True, style={"width": "100%", "height": "auto", "color": "#0D5257", "fill": "#3B7F7F"}),
                         dbc.CardBody(
                             [
                                 html.H4("Pontus Johansson", className="card-title"),
-                                html.P(
-                                    [
-                                        "Kontakta via ",
-                                        html.A("LinkedIn", href="Länk till Adam LinkedIn eller portfolio"),
-                                        html.A("GitHub", href="Länk till Adam GitHub"), ]
+                                html.A(
+                                    href="https://www.linkedin.com/in/pontuskungsbacka/",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Pontus Johansson Linkedin",
+                                            src="/assets/img/linkedin-logo.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
                                 ),
-                            ]
-                        ),
+                                html.A(
+                                    href="https://github.com/pontuskungsbacka/",
+                                    children=[
+                                        html.Img(
+                                            alt="Länk till Pontus Johansson Github",
+                                            src="/assets/img/github-mark.svg",
+                                            width=30, height=30, style={"margin-right": "10px"},
+                                        ),
+                                    ]
+                                ),
+                            ], className="mh-100", style={"height": "8rem"}
+                        ),]
                     ),
                     class_name="mb-3",
                     md=3,
